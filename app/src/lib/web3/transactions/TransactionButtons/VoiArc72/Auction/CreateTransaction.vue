e783ea9a281bad367126231bdd2d78ecad314a7a
kilagg <audranthomas69@gmail.com>
2024-04-27 21:28:18 +0200

    new version auction without fees address

--- app/src/lib/web3/transactions/TransactionButtons/VoiArc72/Auction/CreateTransaction.vue
+++ app/src/lib/web3/transactions/TransactionButtons/VoiArc72/Auction/CreateTransaction.vue
@@ -46,7 +46,6 @@ async function create() {
       longToByteArray(props.parameters.nftAppID, 8),
       longToByteArray(props.parameters.nftID, 32),
       longToByteArray(_reserve, 8),
-      algosdk.decodeAddress(props.parameters.feesAddress).publicKey,
       longToByteArray((Date.now() + endDate.value * 3_600_000) / 1_000, 8)
     ]
 
