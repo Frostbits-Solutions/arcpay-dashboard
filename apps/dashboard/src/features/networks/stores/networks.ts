5125664bad9261e1549123b6b9a36449aaaddac5
n0lk <89748313+nzerolk@users.noreply.github.com>
2025-11-30 18:05:39 -0500

    Remove wallet provider from dashboard network store

--- apps/dashboard/src/features/networks/stores/networks.ts
+++ apps/dashboard/src/features/networks/stores/networks.ts
@@ -5,45 +5,28 @@ import { getNetworks } from '@/services/networks'
 import { getCurrencies } from '@/services/currencies'
 import { errorHandler } from '@/lib/errorHandler'
 import { services } from '@/services/networks'
-import { walletProviders } from '@/features/networks/walletProviders'
 import { supabase } from '@repo/supabase/client'
-import { type SupportedWallet, type NetworkId, WalletManager } from '@txnlab/use-wallet'
 
 export const useNetworksStore = defineStore('networks', () => {
   const networks = ref<Record<string, Network>>({})
   const activeNetworkId = ref<string | undefined>()
   const activeNetworkCurrencies = ref<Currency[]>([])
-  const activeNetworkWalletManager = ref<WalletManager>()
 
   const activeNetwork = computed(() => {
-    if (!activeNetworkId.value || !activeNetworkWalletManager.value) return undefined
+    if (!activeNetworkId.value) return undefined
     return {
       ...networks.value[activeNetworkId.value],
       currencies: activeNetworkCurrencies.value,
       services: services[activeNetworkId.value],
-      walletProvider: activeNetworkWalletManager.value,
     }
   })
 
   const supportedNetworkIds = computed(() => Object.keys(networks.value))
-
   async function setActive(networkId: string, setDefault?: boolean) {
     try {
       if (!networks.value[networkId]) throw new Error(`Network ${networkId} is not supported`)
       if (!services[networkId]) throw new Error(`Network ${networkId} not supported: Missing services`)
-      if (!walletProviders[networkId]) throw new Error(`Network ${networkId} not supported: Missing wallet providers`)
       await fetchCurrencies(networkId)
-      const walletManager = new WalletManager({
-        wallets: walletProviders[networkId] as SupportedWallet[],
-        algod: {
-          token: networks.value[networkId].node_token || '',
-          baseServer: networks.value[networkId].node_url,
-          port: networks.value[networkId].node_port,
-        },
-      })
-      await walletManager.setActiveNetwork(networks.value[networkId].id as NetworkId)
-      await walletManager.resumeSessions()
-      activeNetworkWalletManager.value = walletManager
       activeNetworkId.value = networkId
       if (setDefault !== false) localStorage.setItem('defaultNetwork', networkId)
     } catch (e) {
